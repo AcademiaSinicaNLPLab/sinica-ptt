a009bd8c9f5476e11a897631395152ec51190d89
maxis <maxis1718@gmail.com>
2014-10-14 11:55:56 +0800

    Add path support

--- SinicaPTT/preprocessing.py
+++ SinicaPTT/preprocessing.py
@@ -1,15 +1,16 @@
 # coding: utf-8
 import os, json, sys, pymongo
 
-class ptt(object):
+
+class DataHandler(object):
     """
     Process ptt data for sinica demo
 
     Usage
     =====
-    >> from SinicaPTT import SinicaPTT
-    >> ptt = SinicaPTT()
-    >> ptt.insert(src='articles', dest='HatePolitics')
+    >> from SinicaPTT.preprocessing import DataHandler
+    >> dh = DataHandler()
+    >> dh.insert(src='articles', dest='HatePolitics')
     """
     def __init__(self, **kwargs):
 
@@ -17,7 +18,7 @@ class ptt(object):
         mongo_addr = 'doraemon.iis.sinica.edu.tw' if 'mongo_addr' not in kwargs else kwargs['mongo_addr']
         mongo_db = 'ptt' if 'mongo_db' not in kwargs else kwargs['mongo_db']
         
-        self.db = pymongo.Connection(mongo_addr)[mongo_db]
+        self._db = pymongo.Connection(mongo_addr)[mongo_db]
 
     def insert(self, src, dest):
         """
@@ -29,17 +30,21 @@ class ptt(object):
         dest: str
             the destination collection name
 
+        Usage
+        =====
+        dh.insert(src='articles', dest='HatePolitics')
+
         """
         ## source (folder)
         # src = 'articles'
         ## destination (mongodb)
         # dest = 'HatePolitics'
 
-        co = self.db[dest]
+        co = self._db[dest]
         # filter out all files ending with '.jso'
         for fn in filter(lambda x:x.endswith('.jso'), os.listdir(src)):
             # read article from json files
-            article = json.load(open(fn))
+            article = json.load( open(os.path.join(src, fn)) )
 
             # replace the field `_id` with `fn`
             # because `_id` is a special filed in mongodb
@@ -49,10 +54,11 @@ class ptt(object):
             # insert this document if it is a new one
             if not co.find_one({'fn': article['fn']}):
                 co.insert(article)
-                print 'insert', fn
+                print '> insert', fn
             else:
-                print 'skip', fn
-
+                print '# skip', fn
+        
+        print 'creating index on the filed `fn`'
         co.create_index('fn')
 
 
